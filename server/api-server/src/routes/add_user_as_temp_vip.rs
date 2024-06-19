9189118ff1031d9e34675eda0602dc973cac0ba0
Tristan <tristan@tnet.moe>
2024-06-19 17:38:23 +0200

    fix(api-server): final fix

--- server/api-server/src/routes/add_user_as_temp_vip.rs
+++ server/api-server/src/routes/add_user_as_temp_vip.rs
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 //! POST `/api/addUserAsTempVip`
 use axum::{
-    extract::{Json, Query},
-    http::StatusCode,
+    extract::Query, http::StatusCode, response::Response
 };
+#[allow(unused_imports)]
 use sb_api_types::sb::add_user_as_temp_vip::{
     AddUserAsTempVIPRequestQuery, AddUserAsTempVIPResponseBody,
 };
 
 pub async fn post_add_user_as_temp_vip(
     Query(_query): Query<AddUserAsTempVIPRequestQuery>,
-) -> Result<AddUserAsTempVIPResponseBody, StatusCode> {
+) -> Result<Response, StatusCode> {
     Err(StatusCode::NOT_IMPLEMENTED)
 }
