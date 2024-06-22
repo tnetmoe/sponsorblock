3d7a1265e423e0798a261ddd1d0418679a89ed7e
Tristan <tristan@tnet.moe>
2024-06-22 18:41:17 +0200

    refactor(diesel-sponsor): change to snake_case

--- lib/diesel-sponsor/src/schema.rs
+++ lib/diesel-sponsor/src/schema.rs
@@ -1,34 +1,46 @@
-// @generated automatically by Diesel CLI.
-#![allow(non_snake_case)]
+// SPDX-License-Identifier: AGPL-3.0-only
 
 diesel::table! {
-    archivedSponsorTimes (UUID) {
-        videoID -> Text,
-        startTime -> Float4,
-        endTime -> Float4,
+    archived_sponsor_times (uuid) {
+        #[sql_name = "videoID"]
+        video_id -> Text,
+        #[sql_name = "startTime"]
+        start_time -> Float4,
+        #[sql_name = "endTime"]
+        end_time -> Float4,
         votes -> Int4,
         locked -> Int4,
-        incorrectVotes -> Int4,
-        UUID -> Text,
-        userID -> Text,
-        timeSubmitted -> Int4,
+        #[sql_name = "incorrectVotes"]
+        incorrect_votes -> Int4,
+        #[sql_name = "UUID"]
+        uuid -> Text,
+        #[sql_name = "userID"]
+        user_id -> Text,
+        #[sql_name = "timeSubmitted"]
+        time_submitted -> Int4,
         views -> Int4,
         category -> Text,
-        actionType -> Text,
+        #[sql_name = "actionType"]
+        action_type -> Text,
         service -> Text,
-        videoDuration -> Int4,
+        #[sql_name = "videoDuration"]
+        video_duration -> Int4,
         hidden -> Int4,
         reputation -> Int4,
-        shadowHidden -> Int4,
-        hashedVideoID -> Text,
-        userAgent -> Text,
+        #[sql_name = "shadowHidden"]
+        shadow_hidden -> Int4,
+        #[sql_name = "hashedVideoID"]
+        hashed_video_id -> Text,
+        #[sql_name = "userAgent"]
+        user_agent -> Text,
         description -> Text,
     }
 }
 
 diesel::table! {
-    categoryVotes (id) {
-        UUID -> Text,
+    category_votes (id) {
+        #[sql_name = "UUID"]
+        uuid -> Text,
         category -> Text,
         votes -> Int4,
         id -> Int4,
@@ -43,12 +55,16 @@ diesel::table! {
 }
 
 diesel::table! {
-    lockCategories (id) {
-        videoID -> Text,
-        userID -> Text,
-        actionType -> Text,
+    lock_categories (id) {
+        #[sql_name = "videoID"]
+        video_id -> Text,
+        #[sql_name = "userID"]
+        user_id -> Text,
+        #[sql_name = "actionType"]
+        action_type -> Text,
         category -> Text,
-        hashedVideoID -> Text,
+        #[sql_name = "hashedVideoID"]
+        hashed_video_id -> Text,
         reason -> Text,
         service -> Text,
         id -> Int4,
@@ -57,88 +73,113 @@ diesel::table! {
 
 diesel::table! {
     ratings (id) {
-        videoID -> Text,
+        #[sql_name = "videoID"]
+        video_id -> Text,
         service -> Text,
         #[sql_name = "type"]
         type_ -> Int4,
         count -> Int4,
-        hashedVideoID -> Text,
+        #[sql_name = "hashedVideoID"]
+        hashed_video_id -> Text,
         id -> Int4,
     }
 }
 
 diesel::table! {
-    shadowBannedIPs (hashedIP) {
-        hashedIP -> Text,
+    shadow_banned_ips (hashed_id) {
+        #[sql_name = "hashedIP"]
+        hashed_id -> Text,
     }
 }
 
 diesel::table! {
-    shadowBannedUsers (userID) {
-        userID -> Text,
+    shadow_banned_users (user_id) {
+        #[sql_name = "userID"]
+        user_id -> Text,
     }
 }
 
 diesel::table! {
-    sponsorTimes (UUID) {
-        videoID -> Text,
-        startTime -> Float4,
-        endTime -> Float4,
+    sponsor_times (uuid) {
+        #[sql_name = "videoID"]
+        video_id -> Text,
+        #[sql_name = "startTime"]
+        start_time -> Float4,
+        #[sql_name = "endTime"]
+        end_time -> Float4,
         votes -> Int4,
         locked -> Int4,
-        incorrectVotes -> Int4,
-        UUID -> Text,
-        userID -> Text,
-        timeSubmitted -> Int4,
+        #[sql_name = "incorrectVotes"]
+        incorrect_votes -> Int4,
+        #[sql_name = "UUID"]
+        uuid -> Text,
+        #[sql_name = "userID"]
+        user_id -> Text,
+        #[sql_name = "timeSubmitted"]
+        time_submitted -> Int4,
         views -> Int4,
         category -> Text,
-        actionType -> Text,
+        #[sql_name = "actionType"]
+        action_type -> Text,
         service -> Text,
-        videoDuration -> Int4,
+        #[sql_name = "videoDuration"]
+        video_duration -> Int4,
         hidden -> Int4,
         reputation -> Int4,
-        shadowHidden -> Int4,
-        hashedVideoID -> Text,
-        userAgent -> Text,
+        #[sql_name = "shadowHidden"]
+        shadow_hidden -> Int4,
+        #[sql_name = "hashedVideoID"]
+        hashed_video_id -> Text,
+        #[sql_name = "userAgent"]
+        user_agent -> Text,
         description -> Text,
     }
 }
 
 diesel::table! {
-    thumbnailTimestamps (UUID) {
-        UUID -> Text,
+    thumbnail_timestamps (uuid) {
+        #[sql_name = "UUID"]
+        uuid -> Text,
         timestamp -> Nullable<Int4>,
     }
 }
 
 diesel::table! {
-    thumbnailVotes (UUID) {
-        UUID -> Text,
+    thumbnail_votes (uuid) {
+        #[sql_name = "UUID"]
+        uuid -> Text,
         votes -> Nullable<Int4>,
         locked -> Nullable<Int4>,
-        shadowHidden -> Nullable<Int4>,
+        #[sql_name = "shadowHidden"]
+        shadow_hidden -> Nullable<Int4>,
         downvotes -> Nullable<Int4>,
         removed -> Nullable<Int4>,
     }
 }
 
 diesel::table! {
-    thumbnails (UUID) {
+    thumbnails (uuid) {
         original -> Nullable<Int4>,
-        userID -> Text,
+        #[sql_name = "userID"]
+        user_id -> Text,
         service -> Text,
-        hashedVideoID -> Text,
-        timeSubmitted -> Int4,
-        UUID -> Text,
+        #[sql_name = "hashedVideoID"]
+        hashed_video_id -> Text,
+        #[sql_name = "timeSubmitted"]
+        time_submitted -> Int4,
+        #[sql_name = "UUID"]
+        uuid -> Text,
     }
 }
 
 diesel::table! {
-    titleVotes (UUID) {
-        UUID -> Text,
+    title_votes (uuid) {
+        #[sql_name = "UUID"]
+        uuid -> Text,
         votes -> Int4,
         locked -> Int4,
-        shadowHidden -> Int4,
+        #[sql_name = "shadowHidden"]
+        shadow_hidden -> Int4,
         verification -> Nullable<Int4>,
         downvotes -> Int4,
         removed -> Int4,
@@ -146,67 +187,86 @@ diesel::table! {
 }
 
 diesel::table! {
-    titles (UUID) {
-        videoID -> Text,
+    titles (uuid) {
+        #[sql_name = "videoID"]
+        video_id -> Text,
         title -> Text,
         original -> Nullable<Int4>,
-        userID -> Text,
+        #[sql_name = "userID"]
+        user_id -> Text,
         service -> Text,
-        hashedVideoID -> Text,
-        timeSubmitted -> Int4,
-        UUID -> Text,
+        #[sql_name = "hashedVideoID"]
+        hashed_video_id -> Text,
+        #[sql_name = "timeSubmitted"]
+        time_submitted -> Int4,
+        #[sql_name = "UUID"]
+        uuid -> Text,
     }
 }
 
 diesel::table! {
-    unlistedVideos (id) {
-        videoID -> Text,
+    unlisted_videos (id) {
+        #[sql_name = "videoID"]
+        video_id -> Text,
         year -> Int4,
         views -> Int4,
-        channelID -> Text,
-        timeSubmitted -> Int4,
+        #[sql_name = "channelID"]
+        channel_id -> Text,
+        #[sql_name = "timeSubmitted"]
+        time_submitted -> Int4,
         service -> Text,
         id -> Int4,
     }
 }
 
 diesel::table! {
-    userFeatures (userID, feature) {
-        userID -> Text,
+    user_features (user_id, feature) {
+        #[sql_name = "userID"]
+        user_id -> Text,
         feature -> Int4,
-        issuerUserID -> Text,
-        timeSubmitted -> Int4,
+        #[sql_name = "issuerUserID"]
+        issuer_user_id -> Text,
+        #[sql_name = "timeSubmitted"]
+        time_submitted -> Int4,
     }
 }
 
 diesel::table! {
-    userNames (userID) {
-        userID -> Text,
-        userName -> Text,
+    user_names (user_id) {
+        #[sql_name = "userID"]
+        user_id -> Text,
+        #[sql_name = "userName"]
+        user_name -> Text,
         locked -> Int4,
     }
 }
 
 diesel::table! {
-    videoInfo (videoID) {
-        videoID -> Text,
-        channelID -> Text,
+    video_info (video_id) {
+        #[sql_name = "videoID"]
+        video_id -> Text,
+        #[sql_name = "channelID"]
+        channel_id -> Text,
         title -> Text,
         published -> Float4,
     }
 }
 
 diesel::table! {
-    vipUsers (userID) {
-        userID -> Text,
+    vip_users (user_id) {
+        #[sql_name = "userID"]
+        user_id -> Text,
     }
 }
 
 diesel::table! {
-    warnings (userID, issueTime) {
-        userID -> Text,
-        issueTime -> Int4,
-        issuerUserID -> Text,
+    warnings (user_id, issue_time) {
+        #[sql_name = "userID"]
+        user_id -> Text,
+        #[sql_name = "issueTime"]
+        issue_time -> Int4,
+        #[sql_name = "issuerUserID"]
+        issuer_user_id -> Text,
         enabled -> Int4,
         reason -> Text,
         #[sql_name = "type"]
@@ -214,28 +274,28 @@ diesel::table! {
     }
 }
 
-diesel::joinable!(thumbnailTimestamps -> thumbnails (UUID));
-diesel::joinable!(thumbnailVotes -> thumbnails (UUID));
-diesel::joinable!(titleVotes -> titles (UUID));
+diesel::joinable!(thumbnail_timestamps -> thumbnails (uuid));
+diesel::joinable!(thumbnail_votes -> thumbnails (uuid));
+diesel::joinable!(title_votes -> titles (uuid));
 
 diesel::allow_tables_to_appear_in_same_query!(
-    archivedSponsorTimes,
-    categoryVotes,
+    archived_sponsor_times,
+    category_votes,
     config,
-    lockCategories,
+    lock_categories,
     ratings,
-    shadowBannedIPs,
-    shadowBannedUsers,
-    sponsorTimes,
-    thumbnailTimestamps,
-    thumbnailVotes,
+    shadow_banned_ips,
+    shadow_banned_users,
+    sponsor_times,
+    thumbnail_timestamps,
+    thumbnail_votes,
     thumbnails,
-    titleVotes,
+    title_votes,
     titles,
-    unlistedVideos,
-    userFeatures,
-    userNames,
-    videoInfo,
-    vipUsers,
+    unlisted_videos,
+    user_features,
+    user_names,
+    video_info,
+    vip_users,
     warnings,
 );
